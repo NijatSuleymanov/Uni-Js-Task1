bc6b8d5e9b2de63262ac23623b7aa07004d8ec04
Nijat Suleymanov <109145252+Nicat3333@users.noreply.github.com>
2022-11-21 12:46:26 +0400

    21.11.22 Introdiction -1

---
+++ Uni-js-task1/script.js
@@ -0,0 +1,80 @@
+// var cem=0;
+// for (let a=1;a<10;a++ ) {
+//    cem+=a
+    
+// }
+// console.log(cem);
+
+// var hasil =1;
+// for (let a=1;a<10;a++ ) {
+//     hasil*=a;
+   
+// }
+// console.log(hasil);
+// var cem=0;
+// var hasil=1;
+// for (let a=1; a<100;a++) {
+//     if (a%2==0) {
+//         cem+=a;
+//         hasil*=a;
+//     }
+    
+    
+// }
+// console.log(cem);
+// console.log(hasil);
+// var cem=0;
+// var say=0;
+// for(let a=100;a<1000;a++){
+//    say++
+//    cem+=a
+// }
+// console.log(cem/say);
+
+// for(let a=100;a<1000;a++){
+//     if(a%7==0&&a%8==0){
+//         console.log(a);
+        
+    
+//     }
+//     }
+// for(let a=100;a<1000;a++){
+// if(a%7==0&&a%8==0){
+//     console.log(a);
+//     break;
+
+// }
+// }
+//  var cem =0
+// for(let a=100;a<1000;a++){
+//     if(a%7==0&&a%8==0){
+//        cem+=a
+//     }
+//     }
+//     console.log(cem);
+    
+
+// for (var a = 0; a <= 100; a++) {
+
+//     var notPrime = false;
+//     for (var i = 2; i <= a; i++) {
+//         if (a%i===0 && i!==a) {
+//             notPrime = true;
+//         }
+//     }
+//     if (notPrime === false) {
+//                 console.log(a);
+//     }
+// }
+// var a=0,b=0,c=0,min;
+// a=prompt(" a-ni daxil edin");
+// console.log(a);
+// b=prompt(" b-ni daxil edin");
+// console.log(b);
+// c=prompt(" c-ni daxil edin");
+// console.log(c);
+// min=Math.min(a,b,c)
+// console.log(min);
+
+
+   
